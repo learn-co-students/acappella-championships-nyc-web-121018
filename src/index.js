4237daaf65e549e311c98904eddf67fcefb3eb1d
Oli Nelson <olivernelson@icloud.com>
2019-02-07 10:27:53 -0500

    working on deep hash access for api

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 let allGroups = []
 let tableBody
 let winner
+let createGroupButton
+let formDiv
+let headerDiv
 function apiFetch() {
 fetch("http://localhost:3000/a_cappella_groups")
 .then(r => {
@@ -12,6 +15,28 @@ fetch("http://localhost:3000/a_cappella_groups")
   showAllGroups()
 })
 }//end of apiFetch
+
+function apiCreate(college, groupName, membership, division) {
+  fetch("http://localhost:3000/a_cappella_groups", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify({
+      college:{
+        name: name
+      },
+      name: groupName,
+      membership: membership,
+      division: division,
+
+    })
+  })
+  .then(function(r){
+    return r.json
+  })
+}
 
 function showAllGroups() {
   for (let group of allGroups){
@@ -33,15 +58,35 @@ function getGroupById(id){
   return foundGroup
 }
 
+function showTableItem(id){
+    document.querySelector(`#table-row-${id}`).style.display = ""
+}
+
 function showWinner(id) {
   let g = getGroupById(id)
-
+  winner.dataset.id = id
   winner.innerText = `Winner: ${g.name}`
 }
 
+function showForm(dataAction, college, groupName, membership, division) {
+  formDiv.innerHTML +=
+  `
+  <form id="${dataAction}-form">
+    <input id="${dataAction}-college" placeholder="College" value="">
+    <input id="${dataAction}-groupName" placeholder="GroupName" value="">
+    <input id="${dataAction}-membership" placeholder="Membership" value="">
+    <input id="${dataAction}-division" placeholder="Division" value="">
+    <button> Submit </button>
+  </form>
+  `
+}
+
 document.addEventListener('DOMContentLoaded', e =>{
+  createGroupButton = document.querySelector("#create-group-button")
   tableBody = document.querySelector("#table-body")
   winner = document.querySelector("#winner")
+  formDiv = document.querySelector("#form-div")
+  headerDiv = document.querySelector("#form-div")
   apiFetch()
 
 tableBody.addEventListener('click', function(e){
@@ -49,11 +94,42 @@ tableBody.addEventListener('click', function(e){
   document.querySelectorAll('.tb').style
 
   if (e.target.className === "crown-winner-image"){
+    let currentDisplayedWinnerId = winner.dataset.id
+    if (currentDisplayedWinnerId.length) {
+      showTableItem(currentDisplayedWinnerId)
+    }
+
+    //
     id = e.target.dataset.id
     showWinner(id)
     document.querySelector(`#table-row-${id}`).style.display = "none"
   }
+
 })
+
+createGroupButton.addEventListener('click',function(e){
+  console.log("you clicked create");
+
+  if (formDiv.innerHTML.length){
+    formDiv.innerHTML = ""
+  }else{
+    showForm("create", "","","","")
+  }
+
+}) // end of create toggle button listener
+
+formDiv.addEventListener('submit',function(e){
+  e.preventDefault()
+  console.log("you are submitting");
+  college = document.querySelector('#create-college').value
+  groupName = document.querySelector('#create-groupName').value
+  membership = document.querySelector('#create-membership').value
+  division = document.querySelector('#create-division').value
+  apiCreate(college, groupName, membership, division)
+  // console.log(college, groupName, membership, division);
+})
+
+
 
 
 
